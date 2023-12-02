b16540f8a2249673c2ac27ada2eb281ed5361315
Fombi Favour <77404317+Fombi-Favour@users.noreply.github.com>
2023-12-02 11:37:28 +0100

    Update enumerate.rb

--- enumerate.rb
+++ enumerate.rb
@@ -1,10 +1,21 @@
-def any?
-  return false unless block_given?
-  each { |para| return true if yield(para) }
-  false
-end
-def filter
-  result = []
-  each { |para| result << para if yield(para) }
-  result
+module MyEnumerable
+  def all?
+    return false unless block_given?
+
+    each { |para| return false unless yield(para) }
+    true
+  end
+
+  def any?
+    return false unless block_given?
+
+    each { |para| return true if yield(para) }
+    false
+  end
+
+  def filter
+    result = []
+    each { |para| result << para if yield(para) }
+    result
+  end
 end
